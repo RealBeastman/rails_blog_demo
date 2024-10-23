ead3d68a8d487c5ada91bdf5c244b10d80fe798d
Joshua Eastman <joshuabeastman@gmail.com>
2024-10-23 15:18:02 -0500

    adds post scaffold, adds posts link to navbar, adds toast alerts with animation

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  # Resources for posts
+  resources :posts
 
-  # Pages Routes
+  # Resources for pages
   get 'about', to: 'pages#about'
 
-  # Defines the root path route ("/")
+  # Root path, localhost:3000
   root "pages#home"
 end
