480da006db3a335b1e5c06f5e7b9f9adf448dc6a
Joshua Eastman <joshuabeastman@gmail.com>
2024-10-23 19:06:33 -0500

    adds user functionality through devise, adds links to sign up, sign in, and edit account, starts work on adding comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,20 @@
 Rails.application.routes.draw do
-  devise_for :users
+  get 'users/profile'
+
+  # Resources for devise
+  devise_for :users, controllers: {
+    sessions: "users/sessions",
+    registrations: "users/registrations"
+  }
+
+  # Resources for single user
+  get "/u/:id", to: "users#profile", as: "user"
+
   # Resources for posts
-  resources :posts
+  resources :posts do
+    # Resources for comments - /post/:id/comments/:id
+    resources :comments
+  end
 
   # Resources for pages
   get 'about', to: 'pages#about'
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,9 @@
 title = "Lorem Ipsum"
 loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
+User.create(email: "test@seeded.com", name: "Testman", password: "password", password_confirmation: "password")
+User.create(email: "josh@test.com", name: "Joshua Eastman", password: "password", password_confirmation: "password")
+
 5.times do |x|
-  Post.create(title: "#{title} No. #{x + 1}", body: loremIpsum)
+  Post.create(title: "#{title} No. #{x + 1}", body: loremIpsum, user_id: User.first.id)
 end
